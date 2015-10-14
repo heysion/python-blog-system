275a33a503324ec10326a4bc785cd92406f4c654
Heysion <heysion@users.noreply.github.com>
2015-10-14 23:04:37 +0800

    update tests region  Changes to be committed: 	modified:   tests/test_1.py

--- pbsv/tests/test_1.py
+++ pbsv/tests/test_1.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
-class DBError(object):
+class PBError(object):
     def __init__(self,str):
         self.__error_str = str
     def __str__(self):
         return self.__error_str
 
-class DBKeyError(DBError):
+class PBErrorKey(PBError):
     pass
+
+class PBErrorDB(PBError):
+    pass
+
+class PBType(object):
+    _type = "dbtype"
+    def __init__(self,keyname):
+        raise PBErrorKey("not found key name")
+
+    @property
+    def type(self):
+        return self._type
+
+class PBTypeString(PBType):
+    _type = "dbstring"
+    def __init__(self,keyname):
+        pass
+    def __str__(self):
+        return self._type
+    def __str__():
+        return PBTypeString._type
+
+class PBTypeHash(PBType):
+    _type = "dbhash"
+    def __init__(self,keyname):
+        pass
 
 class DBRegion(object):
     __name__ = "top"
@@ -17,22 +43,10 @@ class DBRegion(object):
         self._zone_name = zonename
         pass
 
-class DBType(object):
-    _type = "dbtype"
-    def __init__(self,keyname):
-        raise DBKeyError("not found key name")
-    @property
-    def type(self):
-        return self.__class__._type
-
-class DBTypeString(DBType):
-    _type = "dbstring"
-    def __init__(self,keyname):
-        pass
-
-class DBTypeHash(DBType):
-    _type = "dbhash"
-    def __init__(self,keyname):
+    def search(self,pb,keyname=None,function=None,data=None):
+        if(pb.db == None):raise PBErrorDB("data base unconfigure")
+        if(pb.type == PBTypeString.type):
+            return db.get(keyname)
         pass
 
 class Test:
@@ -47,8 +61,9 @@ class Test:
 if __name__ == "__main__":
     t1 = Test()
     t1.show()
-    t2 = DBTypeString("abc")
+    t2 = PBTypeString("abc")
     print(t2.type)
-    t3 = DBTypeHash("ab")
+    t3 = PBTypeHash("ab")
     print(t3.type)
+    print(PBTypeHash.type)
 
